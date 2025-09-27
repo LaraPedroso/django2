5861c53590ef8c9d5dcd36a931510e09017fe707
Lara <pedroso5@hotmail.com>
2025-09-27 11:40:25 -0300

    Implementa formulário de contato com validação e mensagens de sucesso/erro

--- core/views.py
+++ core/views.py
@@ -1,12 +1,37 @@
+from django.contrib import messages
 from django.shortcuts import render
 
-# Create your views here.
+from .forms import ContatoForm
+
 
 def index(request):
     return render(request, 'index.html')
 
 def contato(request):
-    return render(request, 'contato.html')
+    form = ContatoForm(request.POST or None)
+
+    if str(request.method) == 'POST':
+        if form.is_valid():
+            # pegando os dados com cleaned_data
+            nome = form.cleaned_data['nome']
+            email = form.cleaned_data['email']
+            assunto = form.cleaned_data['assunto']
+            mensagem = form.cleaned_data['mensagem']
+
+            print('Mensagem enviada')
+            print(f'{nome}')
+            print(f'{email}')
+            print(f'{assunto}')
+            print(f'{mensagem}')
+            
+            messages.success(request, 'Email enviado com sucesso!') 
+            form = ContatoForm()
+        else :
+            messages.error(request, 'Erro ao enviar o Email')
+    context = {
+        'form': form
+    }
+    return render(request, 'contato.html', context)
 
 def produto(request):
     return render(request, 'produto.html')
